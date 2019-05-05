02f2a566b87c56a512c11172f086e06cb1a55945
DraColaTGS <50123084+DraColaTGS@users.noreply.github.com>
2019-05-05 11:13:20 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -59,29 +59,9 @@ client.on('guildCreate', guild => {
 
  
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
-   client.user.setActivity("&help | &invite",{type: 'WATCHING'})
-  console.log('')
-  console.log('')
-  console.log('╔[═════════════════════════════════════════════════════════════════]╗')
-  console.log(`[Start] ${new Date()}`);
-  console.log('╚[═════════════════════════════════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════════════════════════════]╗');
-  console.log(`Logged in as * [ " ${client.user.username} " ]`);
-  console.log('')
-  console.log('Informations :')
-  console.log('')
-  console.log(`servers! [ " ${client.guilds.size} " ]`);
-  console.log(`Users! [ " ${client.users.size} " ]`);
-  console.log(`channels! [ " ${client.channels.size} " ]`);
-  console.log('╚[════════════════════════════════════]╝')
-  console.log('')
-  console.log('╔[════════════]╗')
-  console.log(' Bot Is Online')
-  console.log('╚[════════════]╝')
-  console.log('')
-  console.log('')
+    console.log(`Logged in as ${client.user.tag}!`);
+    client.user.setGame(` &help | &invite   `,'https://www.twitch.tv/zya2d_')
+    client.user.setStatus('ldle');
 });
  
 const ytdl = require("ytdl-core");
