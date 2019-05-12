3543694d87e7ed91b76a8c283b940d5f3dd9fba8
DraColaTGS <50123084+DraColaTGS@users.noreply.github.com>
2019-05-12 13:43:33 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -65,7 +65,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : i1Suhaib ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(` ON ${client.guilds.size} Servers - &help,%inv`,"http://twitch.tv/S-F")
+client.user.setGame(` ON ${client.guilds.size} Servers - &help-&inv`,"http://twitch.tv/S-F")
 client.user.setStatus("dnd")
 });
 client.on("guildCreate", guild => {
